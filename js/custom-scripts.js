9bab764c79988f67f17f9e452c985e805acbeeb3
bIropka <biropka@gmail.com>
2016-08-01 12:51:19 +0300

    v 0.2

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -1,3 +1,20 @@
 $(document).ready(function () {
 
+
+    /** scripts for sliders **/
+
+    $('.slider-services').slick({
+
+    });
+
+    $('.slider-reviews').slick({
+
+    });
+
+    $('.slider-certificates').slick({
+
+    });
+
+    /** end of scripts for sliders **/
+
 });
